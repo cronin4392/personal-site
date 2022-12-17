4096c196248c85e453dead640d5e352ce58d579a
Stephen Cronin <stephen.cronin@tulip.co>
2022-12-17 01:27:24 -0500

    Add some basic homepage content and styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+	<h1>Hi, <br />I’m Stephen Cronin</h1>
+	<p>
+		I am a senior software developer with an eye for design. Throughout my career I have worked
+		across a wide variety of industries and team compositions. I have extensive experience in web
+		development with a focus on the front-end. I pride myself in my ability to work well in a team
+		setting and meeting business goals with realistic technical solutions.
+	</p>
+</header>
+<section>
+	I currently work at <a href="https://tulip.co" target="_blank" rel="noreferrer">Tulip</a> as a
+	software engineer. During the night I am an
+	<a href="https://www.visualsss.net" target="_blank" rel="noreferrer">amatuer VJ artist</a>,
+	creating original realtime graphics for live music acts.
+</section>
+
+<style>
+	h1 {
+		font-size: 3em;
+		line-height: 1.1;
+		font-weight: 400;
+		margin-top: 1.75em;
+		margin-bottom: 1.75em;
+	}
+</style>
