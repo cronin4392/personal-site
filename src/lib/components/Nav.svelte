aaf5d6f25a7ebe874dc4d0b71e08643d4d2fb847
Stephen Cronin <stephen.cronin@tulip.co>
2023-01-03 23:37:06 -0500

    Clean up CSS variables

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -27,7 +27,7 @@
 		top: 0;
 		left: 0;
 		right: 0;
-		background-color: rgba(255, 234, 184, 0.95);
+		background-color: var(--color-yellow-faded);
 		padding: 0.25rem var(--site-padding);
 		z-index: 10;
 
