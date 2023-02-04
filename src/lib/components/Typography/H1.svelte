3155bcb0ae0ab358bd302adea18ad9a54d0eb635
Stephen Cronin <stephen.cronin@tulip.co>
2023-02-03 20:43:24 -0500

    Undo H1 styles

--- src/lib/components/Typography/H1.svelte
+++ src/lib/components/Typography/H1.svelte
@@ -6,7 +6,6 @@
 	<span class="blur-bg">
 		<slot />
 	</span>
-	<span class="swash-text"><slot /></span>
 </svelte:element>
 
 <style>
@@ -15,26 +14,9 @@
 		font-family: var(--font-sans);
 		font-stretch: expanded;
 		font-weight: 400;
-		text-align: right;
+		text-align: left;
 		margin-top: 2rem;
 		margin-bottom: 2rem;
 		position: relative;
-	}
-
-	.swash-text {
-		--shadow-inset: 1px;
-		--shadow-blur: 4px;
-		position: absolute;
-		bottom: -2rem;
-		right: 0;
-		z-index: -3;
-		font-family: 'Cloister Swash';
-		font-size: 15rem;
-		line-height: 1.1;
-		font-weight: 400;
-		color: transparent;
-		text-shadow: var(--shadow-inset) var(--shadow-inset) var(--shadow-blur) var(--color-yellow),
-			0 0 0 var(--color-yellow-dark),
-			var(--shadow-inset) var(--shadow-inset) var(--shadow-blur) var(--color-yellow);
 	}
 </style>
