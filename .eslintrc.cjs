3667add37dd6fbf187f907699df63e02bfe3a3cd
Stephen Cronin <stephen.cronin@tulip.co>
2023-08-15 22:55:44 -0400

    Fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,7 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
+	plugins: ['@typescript-eslint'],
 	rules: {
 		'@typescript-eslint/no-unused-vars': [
 			'warn',
@@ -14,13 +13,19 @@ module.exports = {
 			},
 		],
 	},
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript'),
-	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte'],
 	},
 	env: {
 		browser: true,
