d26ebbd0bc44643a0d1beaccd2585d67a9ed8be9
Stephen Cronin <638332+cronin4392@users.noreply.github.com>
2023-08-15 19:58:20 -0700

    Update packages (#2)
    * Update Svelte packages
    
    * Update rest of packages
    
    * Update lock file
    
    * Fix eslint

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
-import importAssets from 'svelte-preprocess-import-assets';
+import { importAssets } from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
