0e2575ec483d05ed822848b7610bf445b9d16c12
Stephen Cronin <stephen.cronin@tulip.co>
2023-12-17 18:00:55 -0500

    New homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,125 @@
 <script lang="ts">
-	import Layout from '$lib/layouts/Page.svelte';
-	import Link from '$lib/components/Link.svelte';
-	import LinkList from '$lib/components/LinkList.svelte';
-
-	const employers = [
+	type Job = {
+		company: string;
+		title: string;
+		start: string;
+		end: string;
+	};
+	const jobs = [
 		{
-			text: 'Artists For Humanity',
-			href: 'https://afhboston.org',
+			company: 'Tulip Inc.',
+			title: 'Level 4 Software Engineer',
+			start: '12/2022',
+			end: 'NOW',
 		},
 		{
-			text: 'Reaktor',
-			href: 'https://reaktor.com',
+			company: 'VISUALSSS',
+			title: 'VJ and Projection Artist',
+			start: '02/2019',
+			end: 'NOW',
 		},
 		{
-			text: 'The Outline',
-			href: 'https://theoutline.com',
+			company: 'Artists For Humanity',
+			title: 'Director of Creative Technology',
+			start: '09/2020',
+			end: '06/2022',
 		},
 		{
-			text: 'Code and Theory',
-			href: 'https://codeandtheory.com',
+			company: 'Reaktor Inc.',
+			title: 'Senior Developer',
+			start: '07/2018',
+			end: '09/2020',
 		},
-	];
-
-	const friends = [
-		{ text: 'General Type', href: 'https://www.generaltypestudio.com/' },
-		{ text: 'Atomic Farmer', href: 'https://atomicfarmer.com/' },
-		{ text: '', href: 'https://www.fontdue.com/' },
-		{ text: 'Dave Lucia', href: 'https://davelucia.com/' },
+		{
+			company: 'The Outline',
+			title: 'Lead Front-end Developer',
+			start: '09/2016',
+			end: '08/2018',
+		},
+		{
+			company: 'The Intercept',
+			title: 'Senior Web Developer',
+			start: '06/2016',
+			end: '09/2016',
+		},
+		{
+			company: 'HYPERHYPER / PHHHOTO',
+			title: 'Senior Web Developer',
+			start: '01/2015',
+			end: '05/2016',
+		},
+		{
+			company: 'Code & Theory',
+			title: 'Junior > Senior Front-end Developer',
+			start: '09/2011',
+			end: '08/2014',
+		},
 	];
 </script>
 
-<Layout image={1}>
-	<svelte:fragment slot="title">Hi, I’m <br />Stephen Cronin</svelte:fragment>
-	<svelte:fragment slot="intro">
-		<p>
-			I am a software developer with an eye for design. Throughout my career I have worked across a
-			wide variety of industries and team compositions. I have extensive experience in web
-			development with a focus on the front-end. I pride myself in my ability to work well in a team
-			setting and meeting business goals with realistic technical solutions.
-		</p>
-	</svelte:fragment>
-	<svelte:fragment slot="content">
-		<p class="swash">
-			I currently work at <Link href="https://tulip.co">Tulip</Link> as a software engineer. During the
-			night I am an
-			<Link href="https://www.visualsss.net">amatuer VJ artist</Link>, creating original realtime
-			graphics for live music acts. I’ve previously worked for
-			<LinkList links={employers} /> ❧
-		</p>
-	</svelte:fragment>
-</Layout>
+<header>
+	<h1>Stephen Cronin</h1>
+</header>
+
+<section>
+	<h2>Work<br />Experience</h2>
+	<div>
+		<ol>
+			{#each jobs as { company, title, start, end }, i}
+				<li class="job container">
+					<h3>{company}</h3>
+					<p>{title}</p>
+					<div>{start} - {end}</div>
+				</li>
+			{/each}
+		</ol>
+	</div>
+</section>
 
 <style lang="scss">
-	.swash {
-		--link-font: 'Cloister Swash';
+	header,
+	section {
+		min-height: calc(100vh - 14.4vw);
+	}
+	.container {
+		padding-left: 80px;
+		padding-right: 80px;
+	}
+	h1,
+	h2 {
+		font-size: 14.4vw;
+		line-height: 0.8;
+		text-transform: uppercase;
+		color: #000000;
+		position: sticky;
+		z-index: -1;
+		top: 0;
+		font-stretch: normal;
+	}
+
+	h3 {
+		font-size: 1rem;
+		font-weight: 400;
+	}
+
+	ol,
+	ul {
+		list-style: none;
+		padding: 0;
+	}
+
+	li {
+		border-bottom: 1px solid currentColor;
+		padding-top: 1rem;
+		padding-bottom: 1rem;
+
+		&:first-child {
+			border-top: 1px solid currentColor;
+		}
+	}
+
+	.job {
+		display: grid;
+		grid-template-columns: 2fr 2fr 1fr;
 	}
 </style>
